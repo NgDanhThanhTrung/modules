5673a745248f3ba2918c33e41bcb01fd50ad47f8
NgDanhThanhTrung <thanhtrungnguyendanh@gmail.com>
2026-01-08 20:32:38 +0600

    Update spotify-json.js

--- SPOTIFY/spotify-json.js
+++ SPOTIFY/spotify-json.js
@@ -1,17 +1,10 @@
+console.log(`spotify-json-2023.06.13`);
 let url = $request.url;
-const replacements = {
-    'com:443': 'com',
-    'platform=iphone': 'platform=ipad'
-};
-
-let modified = false;
-for (const [oldVal, newVal] of Object.entries(replacements)) {
-    if (url.includes(oldVal)) {
-        url = url.split(oldVal).join(newVal); // Cách thay thế nhanh không dùng Regex
-        modified = true;
-    }
+if (url.includes('platform=iphone')) {
+    url = url.replace(/platform=iphone/, 'platform=ipad');
+} else {
+    console.log('No processing needed');
 }
-
-if (modified) console.log('Spotify URL updated');
-
-$done({ url });
+$done({
+    url
+});
